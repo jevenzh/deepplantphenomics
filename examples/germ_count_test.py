f6e92ecf0124fe7729b9974bd3a0199669e5074c
Jordan <jubbens@gmail.com>
2017-03-30 12:57:32 -0600

    sorting is not useful because labels aren't alphabetical

--- examples/germ_count_test.py
+++ examples/germ_count_test.py
@@ -6,9 +6,6 @@ dir = './data/germ'
 images = [os.path.join(dir, name) for name in os.listdir(dir) if
           os.path.isfile(os.path.join(dir, name)) & name.endswith('.png')]
 
-# Sort so the outputs match the order in the labels file
-images = sorted(images)
-
 net = networks.germRegressor(batch_size=4)
 y = net.forward_pass(images)
 net.shut_down()
